7e765f971b5527642173f23e7c81542c81f76a6e
Jason Hsieh <jason.plainlog@gmail.com>
2019-12-30 22:51:33 +0800

    Complete main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from crawler import Crawler
 from args import get_args
+from datetime import datetime
 
 
 if __name__ == '__main__':
     args = get_args()
     crawler = Crawler()
-    content = crawler.crawl(args.start_date, args.end_date)
+    contents = crawler.crawl(args.start_date, args.end_date)
+
     # TODO: write content to file according to spec
+
+    with open(args.output, 'w') as f:
+        for content in contents:
+            output = (datetime.strftime(content[0], "%Y-%m-%d"), content[1].replace('"', ' '), content[2].replace('"', ' '))
+
+            f.write(f'{output[0]}, "{output[1]}", "{output[2]}"\n')
